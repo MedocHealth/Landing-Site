6d5a2bc08c9b8c407885947eeeee73bc17792276
Subham Kumar Sharma <subhamrelax45@gmail.com>
2023-04-26 17:11:28 +0530

    Add the form submission

--- server.js
+++ server.js
@@ -46,45 +46,50 @@ app.get("/", function(req,res){
 //     })
     
 // })
-app.post("/send-email", async function(req, res){
-  console.log(req.body);
-  let testAccount = await nodemailer.createTestAccount();
-  const transporter = nodemailer.createTransport({
+// app.post("/send-email", async function(req, res){
+//   console.log(req.body);
+//   let testAccount = await nodemailer.createTestAccount();
+//   const transporter = nodemailer.createTransport({
    
-    host: 'smtp.gmail.com',
-    // service: "gmail",
-    port: 587,
-    secure: false,
-    requireTLS: true,
-    auth:{
-      user: process.env.GMAIL_EMAIL,
-      pass: process.env.PASSWORD
-      // user: 'kenneth99@ethereal.email', //for testing
-      //   pass: 'pDdw9TxWQsWchN3Jtf'
-    }
-    // socketTimeout: 100000
-  });
-  const mailOptions = {
-    from : req.body.email,
-    to: process.env.GMAIL_EMAIL,
-    subject: req.body.subject,
-    text: req.body.message
-   };
-  console.log("m1")
-  transporter.sendMail(mailOptions, (error, info)=> { // fixed the error here
-    if(error){
-      console.log(error);
-      res.send("error");
-    }
-    else{
-      console.log("Email sent: " + info.response)
-      console.log(info.messageId);
-      res.send("done!")
-      res.json(info);
-    }
-  });
-  console.log("m2")
-});
+//     host: 'smtp.gmail.com',
+//     // service: "gmail",
+//     port: 587,
+//     secure: false,
+//     requireTLS: true,
+//     auth:{
+//       user: process.env.GMAIL_EMAIL,
+//       pass: process.env.PASSWORD
+//       // user: 'kenneth99@ethereal.email', //for testing
+//       //   pass: 'pDdw9TxWQsWchN3Jtf'
+//     }
+//     // socketTimeout: 100000
+//   });
+//   const mailOptions = {
+//     from : `${req.body.name} <${process.env.GMAIL_EMAIL}>`,
+//     // from : "subhamindian43@gmail.com",
+    
+//     to: req.body.email,
+//     subject: req.body.subject,
+//     // text: "Hello"
+//     text: req.body.msg
+    
+//    };
+//   console.log("m1")
+//   transporter.sendMail(mailOptions, (error, info)=> { // fixed the error here
+//     if(error){
+//       console.log(error);
+//       res.send("error");
+//     }
+//     else{
+//       console.log("Email sent: " + info.response)
+//       console.log(info.messageId);
+//       res.send(info)
+      
+//     }
+//   });
+//   console.log("m2")
+// });
+console.log("hello")
 
 
 app.listen(3000, function(){
